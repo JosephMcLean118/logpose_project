ccecba8aab1df811598ec079a22451cbfb8d7498
JosephMclean118 <josephjmclean392@gmail.com>
2026-03-08 16:28:51 +0000

    Created forms.py

---
+++ logpose_project/logpose/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from logpose.models import Genre, Game, UserProfile, Review
+from django.contrib.auth.models import User
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ["game", "rating", "body",]
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta: 
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('bio', 'profile_image',)
