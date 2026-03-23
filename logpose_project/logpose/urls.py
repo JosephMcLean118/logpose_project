148d2a03dd42a1a2caa39c5a78e844d29b448389
Enoch Junaid <2967122J@student.gla.ac.uk>
2026-03-23 07:32:33 +0000

    Added create review and individual review pages

--- logpose_project/logpose/urls.py
+++ logpose_project/logpose/urls.py
@@ -5,4 +5,6 @@ app_name = 'logpose'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reviews/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('reviews/create/', views.create_review, name='create_review'),
 ]
--- logpose_project/logpose/views.py
+++ logpose_project/logpose/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from logpose.models import Review, UserProfile
+from logpose.forms import ReviewForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'logpose/base.html')
+
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    profile = UserProfile.objects.filter(user=review.user).first()
+
+    context = {
+        'review': review,
+        'profile': profile,
+    }
+    return render(request, 'logpose/review_detail.html', context)
+
+
+def create_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = User.objects.first()
+            review.save()
+            return redirect('logpose:review_detail', review_id=review.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'logpose/create_review.html', {'form': form})
