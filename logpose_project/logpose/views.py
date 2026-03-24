d3822e1d60a18636fec0022601335295d2aa4e45
Ali Noor <2984616a@student.gla.ac.uk>
2026-03-24 01:10:42 +0000

    Fix views.py imports, fix search/game redirects now

--- logpose_project/logpose/views.py
+++ logpose_project/logpose/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from logpose.models import UserProfile, Review 
+from logpose.models import UserProfile, Game, Genre, Review 
 from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.db.models import Avg, Count
-from logpose.models import Game, Genre, Review
 from logpose.forms import UserForm, UserProfileForm, GameSearchForm, ReviewForm
 
 
@@ -88,10 +86,6 @@ def create_review(request):
 def search_games(request):
     """
     Return filtered render of reviews page template.
-    ------------
-    NOT COMPLETE
-    ------------
-    Will complete once reviews page is made
     """
     form = GameSearchForm(request.GET)
     if form.is_valid():
@@ -99,21 +93,35 @@ def search_games(request):
         genre = form.cleaned_data['genre']
         stars = form.cleaned_data['stars']
         year = str(form.cleaned_data['year'])
-        print(form)
-        return HttpResponse(f"Game: {game_name}, Genre: {genre}, Stars: {stars}, Year: {year}"  )
-    return HttpResponse("Fail ")
+        
+        # Build redirect URL to reviews page with filters
+        url = reverse('logpose:reviews')
+        params = []
+        if game_name:
+            params.append(f'search={game_name}')
+        if genre:
+            params.append(f'genre={genre}')
+        if stars:
+            params.append(f'rating={stars}')
+        if year:
+            params.append(f'year={year}')
+        if params:
+            url += '?' + '&'.join(params)
+        return redirect(url)
+    return redirect('logpose:reviews')
+
 
 def reviews_for_game(request, slug):
     """
     Return render of reviews page, filtered by games name
-    ------------
-    NOT COMPLETE
-    ------------
-    Will complete once reviews page is made
+
     """
     game = get_object_or_404(Game, slug=slug)
-    reviews = game.review_set.all()  # gets all reviews for this game
-    return HttpResponse(f"Game: {game.title}, Reviews: {reviews}")
+    #reviews = game.review_set.all()  # gets all reviews for this game
+    # we dont need to get all the reviews we can just redirect it reversely
+    #  to the reviews page
+    url = reverse('logpose:reviews') + f'?search={game.title}'
+    return redirect(url)
 
 
 
